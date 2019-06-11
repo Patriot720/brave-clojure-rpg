b3f04ef33bc5d14f2c3c93501dfda461ef6a3651
Andrew <cerkin-3@yandex.ru>
2019-06-11 21:56:57 +0300

    dialog choice system

--- src/brave_clojure_rpg/dialog_controller.clj
+++ src/brave_clojure_rpg/dialog_controller.clj
@@ -3,11 +3,13 @@
 
 (defprotocol Dialog
   "Control dialog reactions"
-  (print [x] "Print a dialog"))
+  (print [x] "Print a dialog")
+  (choose [dialog choice] "returns next dialog based on choice"))
 
 (defrecord SimpleDialog [title choices]
   Dialog
   (print [dialog]
     (println title)
     (doseq [choice (keys choices) i (range 1 (+ (count choices) 1))]
-      (println (str i ":" choice)))))
+      (println (str i ":" choice))))
+  (choose [dialog choice] (get choices (nth (keys choices) (- choice 1)))))
--- test/brave_clojure_rpg/dialog_test.clj
+++ test/brave_clojure_rpg/dialog_test.clj
@@ -2,7 +2,11 @@
   (:require [clojure.test :refer [testing deftest is]]
             [brave-clojure-rpg.dialog-controller :as di]))
 
-(deftest start-a-dialog
-  (is (=
-       (with-out-str (di/print (di/->SimpleDialog "lulz" {"Fuck the police" "lulz"})))
-       "lulz\r\n1:Fuck the police\r\n")))
+(let [other_dialog (di/->SimpleDialog "other-dialog" {})
+      dialog (di/->SimpleDialog "lulz" {"Fuck the police" other_dialog})]
+  (deftest print-a-dialog
+    (is (=
+         (with-out-str (di/print dialog))
+         "lulz\r\n1:Fuck the police\r\n")))
+  (deftest choosing-dialog-variant-should-return-new-dialog
+    (is (= (di/choose dialog 1)  other_dialog))))
