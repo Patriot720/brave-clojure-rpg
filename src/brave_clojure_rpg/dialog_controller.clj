25dd5330da3b569e4e80876d47cdb3a3daf83de5
Andrew <cerkin-3@yandex.ru>
2019-06-16 20:25:57 +0300

    add dialog parsing

--- src/brave_clojure_rpg/dialog_controller.clj
+++ src/brave_clojure_rpg/dialog_controller.clj
@@ -1,17 +1,26 @@
 (ns brave-clojure-rpg.dialog-controller
+  (:require [clojure.data.json :as json])
   (:gen-class))
-
+(declare parse-dialog-json)
 (defprotocol Dialog
   "Control dialog reactions"
   (start [dialog])
   (print [x] "Print a dialog")
   (choose [dialog choice] "returns next dialog based on choice"))
 
-(defrecord SimpleDialog [title choices]
+(defrecord SimpleDialog [title description choices]
   Dialog
   (start [dialog] (print dialog) (choose dialog (Integer. (read-line))))
   (print [dialog]
     (println title)
-    (doseq [choice (keys choices) i (range 1 (+ (count choices) 1))]
-      (println (str i ":" choice))))
-  (choose [dialog choice] (get choices (nth (keys choices) (- choice 1)))))
+    (doseq [choice  choices i (range 1 (+ (count choices) 1))]
+      (println (str i ":" (:title choice)))))
+  (choose [dialog choice]  (get choices  (- choice 1))))
+
+(defn parse-dialog-from-file [file]
+  (parse-dialog-json (json/read-str file)))
+
+(defn- parse-dialog-json ([json]
+                          (->SimpleDialog (first json)
+                                          (nth json 1)
+                                          (map (fn [item] (parse-dialog-json item)) (last json)))))
--- test/brave_clojure_rpg/dialog_test.clj
+++ test/brave_clojure_rpg/dialog_test.clj
@@ -2,17 +2,19 @@
   (:require [clojure.test :refer [testing deftest is]]
             [brave-clojure-rpg.dialog-controller :as di]))
 
-(let [other_dialog (di/->SimpleDialog "other-dialog" {})
-      dialog (di/->SimpleDialog "lulz" {"Fuck the police" other_dialog})]
+(let [other_dialog (di/->SimpleDialog "Fuck the police" "other_dialog" [])
+      dialog (di/->SimpleDialog "lulz" "wtf" [other_dialog])]
   (deftest print-a-dialog
     (is (=
          (with-out-str (di/print dialog))
          "lulz\r\n1:Fuck the police\r\n")))
   (deftest choosing-dialog-variant-should-return-new-dialog
     (is (= (di/choose dialog 1)  other_dialog)))
-  (deftest start-dialog-should-return-new-dialog
+  (deftest start-dialog-should-recursively-choose-dialogs
     (with-in-str "1" (is (= (di/start dialog) other_dialog)))))
 
 (deftest parse-dialog-tree-from-file-test
   (let [file (slurp "example_dialog.json")]
-    (di/parse-dialog-from-file file)))
+    (is (= (di/parse-dialog-from-file file)
+           (di/->SimpleDialog "Kill everyone" "You fucked up everyone"
+                              [di/->SimpleDialog "Wtf" "other stuff happened" ()])))))
