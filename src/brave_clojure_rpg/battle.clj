4d5200715bdea17659d13a0919829c809c1b01bf
Andrew <cerkin-3@yandex.ru>
2019-06-20 20:57:51 +0300

    added attack method

--- src/brave_clojure_rpg/battle.clj
+++ src/brave_clojure_rpg/battle.clj
@@ -6,3 +6,9 @@
   (println (str "Your hp is:" (:hp hero)))
   (println (str "His hp is:" (:hp enemy)))
   (println "Your move"))
+
+(defn attack [hero enemy weapon]
+  (assoc enemy :hp
+         (Double/parseDouble (format "%.1f" (-
+                                             (get enemy :hp)
+                                             (float (/ (get-in hero [:weapons weapon]) (reduce  #(+ %1 %2) (vals (get enemy :equipment))))))))))
--- test/brave_clojure_rpg/battle_test.clj
+++ test/brave_clojure_rpg/battle_test.clj
@@ -1,12 +1,14 @@
 (ns brave-clojure-rpg.battle-test
   (:require [clojure.test :refer [testing deftest is]]
             [brave-clojure-rpg.battle :as bt]))
-(def hero {:hp 10})
-(def enemy {:name "gremlin" :hp 1})
+(def hero {:hp 10 :weapons {:spear 3}
+           :equipment {:headgear 3}})
+(def enemy {:name "gremlin" :hp 1 :equipment {:one-true-ring 50}})
 (deftest initalizing-battle-test
   (is (=
        (with-out-str (bt/print-battle-status hero enemy))
        "Started battle with gremlin\r\nYour hp is:10\r\nHis hp is:1\r\nYour move\r\n")))
 
 (deftest attack-test
-  (is (= (bt/attack hero enemy))))
+  (is (= (bt/attack hero enemy :spear)
+         (assoc enemy :hp 0.9))))
