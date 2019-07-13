09adb7f64e66e71d75f52d7ded9060d94eedc67b
Andrew <cerkin-3@yandex.ru>
2019-07-13 22:26:12 +0300

    add yaml parsing

--- src/brave_clojure_rpg/parsing.clj
+++ src/brave_clojure_rpg/parsing.clj
@@ -1,30 +1,33 @@
 (ns brave-clojure-rpg.parsing
   (:require [brave-clojure-rpg.dialogs :as Dialogs])
   (:require [clojure.data.json :as json])
+  (:require [yaml.core :as yaml])
   (:require [brave-clojure-rpg.person :as person]))
 
  ; TODO different condition instead of counting array (dictionary)
  ; TODO add SideEffectDialog
-(defn parse-dialog [hero arr]
-  (let [real-arr (drop 1 arr)]
-    (case (clojure.string/lower-case (nth arr 0 ""))
-      "battle" (Dialogs/->BattleDialog (first real-arr) (nth real-arr 1) hero (person/map->Person (nth real-arr 2)) (parse-dialog hero (last real-arr)))
-      "simple" (Dialogs/->SimpleDialog (first real-arr)
-                                       (nth real-arr 1)
-                                       hero
-                                       (into [] (map
-                                                 (fn [item] (parse-dialog hero item)) (last real-arr))))
-      "sideeffect" (Dialogs/->SideEffectDialog (first real-arr) (nth real-arr 1) hero (nth real-arr 2) (parse-dialog hero (last real-arr)))
-      "" [])))
+(defn parse-dialog
+  ([arr]
+   (parse-dialog (person/map->Person (:hero arr {})) (get arr :dialogs)))
+  ([hero arr]
+   (let [real-arr (drop 1 arr)]
+     (case (clojure.string/lower-case (nth arr 0 ""))
+       "battle" (Dialogs/->BattleDialog (first real-arr) (nth real-arr 1) hero (person/map->Person (nth real-arr 2)) (parse-dialog hero (last real-arr)))
+       "simple" (Dialogs/->SimpleDialog (first real-arr)
+                                        (nth real-arr 1)
+                                        hero
+                                        (into [] (map
+                                                  (fn [item] (parse-dialog hero item)) (last real-arr))))
+       "sideeffect" (Dialogs/->SideEffectDialog (first real-arr) (nth real-arr 1) hero (nth real-arr 2) (parse-dialog hero (last real-arr)))
+       "" []))))
 
 (defmulti parse-dialog-from-file (fn [filename] (last (clojure.string/split filename #"\."))))
 (defmethod parse-dialog-from-file "json"
   [filename]
-  (let [json (json/read-str (slurp filename) :key-fn keyword)
-        hero (person/map->Person (:hero json {}))
-        dialogs (get json :dialogs)]
-    (parse-dialog hero dialogs)))
-
+  (parse-dialog (json/read-str (slurp filename) :key-fn keyword)))
+(defmethod parse-dialog-from-file "yaml"
+  [filename]
+  (parse-dialog (yaml/parse-string (slurp filename) :keywords true)))
 ; (defn parse-dialog-from-file [file]
 ;   (let [json (json/read-str (slurp file) :key-fn keyword)
 ;         hero (person/map->Person (:hero json))]
--- test/brave_clojure_rpg/parsing_test.clj
+++ test/brave_clojure_rpg/parsing_test.clj
@@ -13,7 +13,10 @@
         nested-dialog (get-in expected-dialog [:choices 0])]
 
     (testing "Parsing example_dialog with one nested dialog"
-      (is (=  parsed-dialog expected-dialog)))
+      (testing "JSON"
+        (is (=  parsed-dialog expected-dialog)))
+      (testing "YAML"
+        (is (=  (parse-dialog-from-file "example_dialog.yaml") expected-dialog))))
 
     (testing "Choosing 0 should return nested dialog"
       (is (= (Dialogs/choose parsed-dialog 0)
@@ -22,15 +25,18 @@
   (testing "Battle dialog parsing"
     (let [parsed-dialog
           (parse-dialog-from-file "example_mixed_dialogs.json")
-          hero (->Person "Hero" 20 {:spear 10} nil)]
+          hero (->Person "Hero" 20 {:spear 10} nil)
+          expected (Dialogs/->BattleDialog "Battle with gremlin" ""
+                                           hero
+                                           (->Person "Gremlin" 20 {:hands 25} {})
+                                           (Dialogs/->SimpleDialog
+                                            "Winner" "Chicken dinner" hero
+                                            [(Dialogs/->SideEffectDialog "Damaged" "For 5 dmg" hero 5 [])]))]
       (testing "Should return battle dialog with simple nested win dialog"
-        (is (=  parsed-dialog
-                (Dialogs/->BattleDialog "Battle with gremlin" ""
-                                        hero
-                                        (->Person "Gremlin" 20 {:hands 25} {})
-                                        (Dialogs/->SimpleDialog
-                                         "Winner" "Chicken dinner" hero
-                                         [(Dialogs/->SideEffectDialog "Damaged" "For 5 dmg" hero 5 [])])))))))
+        (testing "JSON"
+          (is (=  parsed-dialog expected)))
+        (testing "YAML"
+          (is (= (parse-dialog-from-file "example_mixed_dialogs.yaml") expected))))))
 
   (testing "Breadth dialogs test"
     (let [parsed-dialog (parse-dialog-from-file "example_breadth_first_dialogs.json")]
