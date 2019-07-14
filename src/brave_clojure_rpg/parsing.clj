cdd7fd1d0756ce877de5910078c0c361d087ff73
Andrew <cerkin-3@yandex.ru>
2019-07-14 17:43:37 +0300

    changed parsing quality

--- src/brave_clojure_rpg/parsing.clj
+++ src/brave_clojure_rpg/parsing.clj
@@ -10,16 +10,15 @@
   ([arr]
    (parse-dialog (person/map->Person (:hero arr {})) (get arr :dialogs)))
   ([hero arr]
-   (let [real-arr (drop 1 arr)]
-     (case (clojure.string/lower-case (nth arr 0 ""))
-       "battle" (Dialogs/->BattleDialog (first real-arr) (nth real-arr 1) hero (person/map->Person (nth real-arr 2)) (parse-dialog hero (last real-arr)))
-       "simple" (Dialogs/->SimpleDialog (first real-arr)
-                                        (nth real-arr 1)
-                                        hero
-                                        (into [] (map
-                                                  (fn [item] (parse-dialog hero item)) (last real-arr))))
-       "sideeffect" (Dialogs/->SideEffectDialog (first real-arr) (nth real-arr 1) hero (nth real-arr 2) (parse-dialog hero (last real-arr)))
-       "" []))))
+   (case (clojure.string/lower-case (:type arr ""))
+     "battle" (Dialogs/->BattleDialog (:title arr) (:description arr) hero (person/map->Person (:enemy arr)) (parse-dialog hero (:win-dialog arr)))
+     "simple" (Dialogs/->SimpleDialog (:title arr)
+                                      (:description arr)
+                                      hero
+                                      (into [] (map
+                                                (fn [item] (parse-dialog hero item)) (:choices arr))))
+     "sideeffect" (Dialogs/->SideEffectDialog (:title arr) (:description arr 1) hero (:side-effect arr 2) (parse-dialog hero (:choices arr)))
+     "" [])))
 
 (defmulti parse-dialog-from-file (fn [filename] (last (clojure.string/split filename #"\."))))
 (defmethod parse-dialog-from-file "json"
@@ -28,6 +27,7 @@
 (defmethod parse-dialog-from-file "yaml"
   [filename]
   (parse-dialog (yaml/parse-string (slurp filename) :keywords true)))
+
 ; (defn parse-dialog-from-file [file]
 ;   (let [json (json/read-str (slurp file) :key-fn keyword)
 ;         hero (person/map->Person (:hero json))]
